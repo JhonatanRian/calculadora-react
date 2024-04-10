922815c8ed2fba2a4a2ac468d698e3e51f0311fc
JhonatanRian <jhonatanrian.jr@gmail.com>
2024-04-09 22:37:18 -0300

    feat: estilizando botões e organizando suas posições

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,4 +2,15 @@ import React from "react";
 import "./Button.css";
 
 
-export default props => <button>{props.label}</button>
+export default props => {
+    let classes = 'button ';
+
+    classes += props.operation ? 'operation ' : ''
+    classes += props.double ? 'double ' : ''
+    classes += props.triple ? 'triple ' : ''
+    return (
+        <button className={classes} onClick={event => props.click && props.click(props.label)}>
+            {props.label}
+        </button>
+    )
+}
